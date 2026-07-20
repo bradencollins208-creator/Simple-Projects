942f9002055bdae7fa27f54c5647cd0812d87bb1
Braden Collins <Bradencollins208@gmail.com>
2026-07-20 01:02:37 -0400

    worked on the todo list program and added the add task function

--- todo_list.py
+++ todo_list.py
@@ -1,1 +1,36 @@
-asf
+tasks = []
+def addTask(value):
+    tasks.append(value)
+def removeTask(value):
+    tasks.remove(value)
+def printTasks():
+    print("To-Do List:")
+    for item in tasks:
+        print("\t- "+item+"\n")
+while True:
+    print("\nMake a to-do list!")
+    print("1. Add Task")
+    print("2. Remove Task")
+    print("3. Mark Task Complete")
+    print("4. Save Tasks")
+    print("0. Exit")
+    choice = input("Enter a number: ")
+    if (choice=="1"):
+        print("Item Entry Mode")
+        while True:
+            task = input("Enter task (enter \"stop\" to stop): ")
+            if (task!="stop"):
+                addTask(task)
+            else:
+                printTasks()
+                break
+    elif choice=="2":
+        pass
+    elif choice=="3":
+        pass
+    elif choice=="4":
+        pass
+    elif choice=="0":
+        break
+    else:
+        print("Invalid")
