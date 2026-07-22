bac263c36aefc2999a26a1e72dfe69ed19ac310d
Braden Collins <Bradencollins208@gmail.com>
2026-07-22 15:19:06 -0400

    began changing layout of code to use dictionary and worked on printing tasks and adding items with new setup

--- todo_list.py
+++ todo_list.py
@@ -1,33 +1,40 @@
 tasks = []
+def printTasks():
+    print("To-Do List:")
+    #for i in range(1,len(tasks)):
+    for item in tasks:
+        print(str(tasks.index(item)+1)+". [ ] "+item["task"])
 def addTask(value):
-    tasks.append(value)
+    tasks.append({"task" : value, "completed" : False})
 def removeTask(value):
     if value in tasks:
         tasks.remove(value)
     else:
         print("Task not in to-do list")
-def markComplete():
-    pass
-def printTasks():
-    print("To-Do List:")
-    for item in tasks:
-        print("\t- "+item+"\n")
+def markComplete(value):
+    if value in tasks:
+        pass
+    else:
+        print("Task not in to-do list")
 while True:
     print("\nMake a to-do list!")
-    print("1. Add Task")
-    print("2. Remove Task")
+    print("1. Show Tasks")
+    print("2. Add Task")
     print("3. Mark Task Complete")
-    print("4. Save Tasks")
+    print("4. Remove Task")
+    print("5. Save")
+    print("6. Load")
     print("0. Exit")
     choice = input("Enter a number: ")
-    if (choice=="1"):
+    if choice=="1":
+        printTasks()
+    elif (choice=="2"):
         print("Item Entry Mode")
         while True:
             task = input("Enter task to add (enter \"stop\" to stop): ")
             if (task!="stop"):
                 addTask(task)
             else:
-                printTasks()
                 break
     elif choice=="2":
         print("Item Removal Mode")
@@ -39,7 +46,17 @@ while True:
                 printTasks()
                 break
     elif choice=="3":
-        pass
+        print("Item Completion Mode")
+        while True:
+            task = input("Enter task to mark complete (enter \"stop\" to stop): ")
+            if (task!="stop"):
+                #for item in tasks:
+                    #if item==task:
+                        #item=markComplete(task)
+                markComplete(task)
+            else:
+                printTasks()
+                break
     elif choice=="4":
         pass
     elif choice=="0":
